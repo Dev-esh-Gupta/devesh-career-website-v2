569fd1300ef28133ae89f17bc681fff7d5c2b2ca
Dev-esh-Gupta <136115380+Dev-esh-Gupta@users.noreply.github.com>
2024-03-19 03:43:17 +0000

    store applications in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -56,7 +56,10 @@ def apply_to_job(id):
   # data = request.args
   # for post method
   data = request.form
-  return jsonify(data)
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  # return jsonify(data)
+  return render_template("application_submitted.html", application=data, job=job)
   
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
+
 engine = create_engine(db_connection_string)
 
 def load_jobs_from_db():
@@ -26,3 +27,18 @@ def load_job_from_db(id):
     else:
       return dict(rows[0]._mapping)
 
+def add_application_to_db(job_id, data):
+  conn = engine.connect()
+  # with engine.connect() as conn:
+  query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :experience, :resume_url)")
+  conn.execute(query, 
+            {
+              "job_id" : job_id,
+              "full_name" : data["full_name"],
+              "email" : data["email"],
+              "linkedin_url" : data["linkedin_url"],
+              "education" : data["education"],
+              "experience" : data["work_experience"],
+              "resume_url" : data["resume_url"]
+            }
+            )
